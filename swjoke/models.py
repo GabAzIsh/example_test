99b3d8e29c425e8e38b34f6b3c11f786ba23e5db
GabAzIsh <cat.iquality@gmail.com>
2020-01-30 17:13:26 +0300

    Upgrade models

--- swjoke/models.py
+++ swjoke/models.py
@@ -1,3 +1,47 @@
 from django.db import models
-
 # Create your models here.
+
+class SithPlanets(models.TextChoices):
+    ASHAS_REE = 'Ashas Ree'
+    ATHISS = 'Athiss'
+    BEGEREN = 'Begeren'
+    BOSTHIRDA = 'Bosthirda'
+    CHHODOS = 'Ch\'hodos'
+    DROMUND_FELS = 'Dromund Fels'
+    DROMUND_IXIN = 'Dromund Ixin'
+    DROMUND_KAAS = 'Dromund Kaas'
+    DROMUND_KALAKAR = 'Dromund Kalakar'
+    KALAKAR_SIX = 'Kalakar Six'
+    DROMUND_TYNE = 'Dromund Tyne'
+    JAGUADA = 'Jaguada'
+    JAGUADAS_MOON = 'Jaguada\'s moon'
+    KALSUNOR = 'Kalsunor'
+    KHAR_DELBA = 'Khar Delba'
+    KHAR_SHIAN = 'Khar Shian'
+    KORRIBAN = 'Korriban'
+    KORRIZ = 'Korriz'
+    KRAYISS_TWO = 'Krayiss Two'
+    NFOLGAI = 'Nfolgai'
+    RHELG = 'Rhelg'
+    ZIOST = 'Ziost'
+
+class Recruit(models.Model):
+
+    recruit_planet = models.CharField(
+        max_length=20,
+        choices=SithPlanets.choices,
+        default=SithPlanets.ASHAS_REE,
+    )
+    recruit_name = models.CharField(max_length=100)
+    recruit_age = models.PositiveIntegerField()
+    recruit_email = models.EmailField(max_length=100)
+
+
+class Sith(models.Model):
+
+    sith_name = models.CharField(max_length=100)
+    sith_planet = models.CharField(
+        max_length=20,
+        choices=SithPlanets.choices,
+        default=SithPlanets.ASHAS_REE,
+    )
