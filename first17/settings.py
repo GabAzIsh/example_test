d43832e6e6ef5a0ba2df3afb58313b53cd092366
GabAzIsh <cat.iquality@gmail.com>
2020-02-12 18:43:50 +0300

    Final ver

--- first17/settings.py
+++ first17/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'reviewdb',
         'USER':'postgres',
-        'PASSWORD': 'Cat.iquality@gmail.com',
+        'PASSWORD': ' ',
         'HOST': '127.0.0.1',
         'PORT': '5432'
     }
