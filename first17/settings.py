aaa7de877794f8a40ffde0f72a39a0d0d44782e8
GabAzIsh <58802792+GabAzIsh@users.noreply.github.com>
2020-03-03 23:15:33 +0300

    Update settings.py

--- first17/settings.py
+++ first17/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'captain.justy.ueki.taylor@gmail.com'
-EMAIL_HOST_PASSWORD = 'Admin_for_swjoke'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
